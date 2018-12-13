86434b3f20ea784331aa37ea9e24ddc6878d8075
Pat Neel <neelp@colorado.edu>
2018-12-13 07:52:56 -0700

    add files for initial set-up

---
+++ src/YearlyChart.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './yearly-chart.css';
+
+const YearlyChart = () => {
+  return (
+    <div>
+      Yearly Chart of Stuff lives here
+    </div>
+  )
+}
+
+export default YearlyChart;
