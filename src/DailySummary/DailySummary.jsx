72dffef6fefa0a80e2a3c246beac72595f0855d0
Pat Neel <neelp@colorado.edu>
2018-12-17 10:16:12 -0700

    add icons for profit or loss

--- src/DailySummary/DailySummary.jsx
+++ src/DailySummary/DailySummary.jsx
@@ -3,6 +3,7 @@ import './daily-summary.css';
 
 const DailySummary = () => (
     <div>
+      <h3>April 10, 2018</h3>
       <h3>Number of Products Sold</h3>
       <h4>125</h4>
       <h3>Total Revenue April 10</h3>
