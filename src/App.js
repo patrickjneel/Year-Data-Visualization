d978371ffef3cad69c6c1d585f2243227765b150
Pat Neel <neelp@colorado.edu>
2018-12-17 08:50:57 -0700

    add weather and orders to to main app component

--- src/App.js
+++ src/App.js
@@ -2,8 +2,15 @@ import React, { Component } from 'react';
 import DailySummary from './DailySummary/DailySummary';
 import YearlyChart from './YearlyChart/YearlyChart';
 import './App.css';
+import weatherData from '../src/db/weather2018.json';
+import orders from '../src/db/orders.json';
 
 class App extends Component {
+
+  componentDidMount() {
+
+  }
+  
   render() {
     return (
       <div className="app">
--- src/DailySummary/DailySummary.jsx
+++ src/DailySummary/DailySummary.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import './daily-summary.css';
 
-const DailySummary = () => {
-  return (
+const DailySummary = () => (
     <div>
       Daily Summary of Stuff Goes Here
     </div>
   )
-}
+
 
 export default DailySummary;
