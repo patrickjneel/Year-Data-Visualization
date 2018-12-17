6cc961d8b0d5aaf6b075195666e818be17c1f710
Pat Neel <neelp@colorado.edu>
2018-12-17 10:35:19 -0700

    add svgs for daily percentage

--- src/DailySummary/DailySummary.jsx
+++ src/DailySummary/DailySummary.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import './daily-summary.css';
+import { TrendingUp, TrendingDown } from '@material-ui/icons';
+
+// <TrendingDown style={{ fill: 'red', height: '50px', width: '50px', float: 'right' }} />
 
 const DailySummary = () => (
-    <div>
-      <h3>April 10, 2018</h3>
-      <h3>Number of Products Sold</h3>
-      <h4>125</h4>
-      <h3>Total Revenue April 10</h3>
-      <h4>$8,000</h4>
+    <div className="daily-summary-container">
+      <div className="left-side-container">
+        <h3>April 10, 2018</h3>
+        <h3>Number of Products Sold</h3>
+        <h4>125</h4>
+        <h3>Total Revenue April 10</h3>
+        <h4>$8,000</h4>
+      </div>
+      <div className="right-side-container">
+        <TrendingUp  style={{ fill: 'green', height: '50px', width: '50px' }}/>
+        <h4 className="daily-percentage">0.78%</h4>
+      </div>
     </div>
   )
 
