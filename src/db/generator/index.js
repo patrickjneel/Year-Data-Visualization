8d28a17d33ff9cfd8f3a412d4dd798d5c2826154
Alex Jones <alexander.t.jones@gmail.com>
2018-12-20 15:30:37 -0700

    Data fix (#15)

--- src/db/generator/index.js
+++ src/db/generator/index.js
@@ -6,121 +6,158 @@ const faker = require('faker');
 const sample = require("lodash.sample");
 const moment = require('moment');
 
-const { productCollection } = require('./productsCollection');
+const {productCollection} = require('./productsCollection');
 
 const PRECISION = 0.01;
 
 const boundingBoxArray = [
-  {
+    {
         lat: {
-          min: -23.18,
-          max: -33.54
+            min: -23.18,
+            max: -33.54
         },
         long: {
             min: -48.75,
             max: -71.164
         },
-      name: 'SA Bottom',
-      lp: 3
+        name: 'SA Bottom',
+        lp: 3
     },
     {
         lat: {
-          min: 39.18,
-          max: 47.54
+            min: 39.18,
+            max: 47.54
         },
         long: {
             min: -123.75,
             max: -93.164
         },
-      name: 'US Main',
-      lp: 5
+        name: 'US Main',
+        lp: 3
     },
     {
         lat: {
-          min: -19.18,
-          max: -31.54
+            min: 47,
+            max: 54
+        },
+        long: {
+            min: -122,
+            max: -102
+        },
+        name: 'Canada Central',
+        lp: 1
+    },
+    {
+        lat: {
+            min: 31,
+            max: 40
+        },
+        long: {
+            min: -81,
+            max: -116
+        },
+        name: 'US East',
+        lp: 2
+    },
+
+
+    {
+        lat: {
+            min: -19.18,
+            max: -31.54
         },
         long: {
             min: 115.75,
             max: 146.164
         },
-      name: 'AUS Main',
-      lp: 3
+        name: 'AUS Main',
+        lp: 3
     },
     {
         lat: {
-          min: -22.18,
-          max: -37.54
+            min: -22.18,
+            max: -37.54
         },
         long: {
             min: 139.75,
             max: 153.164
         },
-      name: 'AUS Bottom',
-      lp: 4
+        name: 'AUS Bottom',
+        lp: 4
     },
 ]
 
-const generateCoordValue = (min, max) => {
-  return faker.random.number({
-    min,
-    max,
-    precision: PRECISION
-  });
+const generateCoordValue = (options) => {
+    let {min, max} = options;
+    return faker.random.number({
+        min,
+        max,
+        precision: PRECISION
+    });
 };
 
 
 const order = (bbItem, date) => {
-  let obj =
-      {
-        "type": "Feature",
-        "geometry": {
-          "type": "Point",
-          "coordinates": [generateCoordValue(), generateCoordValue()]
-        },
-        "properties": {
-          "accountNumber": faker.random.number(),
-          "orderNumber": faker.random.number(),
-          "createdDate": moment(date).format('M/DD/YYYY'),
-          "userFirstName": faker.name.firstName(),
-          "userLastName": faker.name.lastName(),
-          "userEmail": faker.internet.email(),
-          "products": [
-            {
-              "productId": faker.random.number(),
-              "productName": btoa(sample(productCollection)),
+    let obj =
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [generateCoordValue(bbItem.long), generateCoordValue(bbItem.lat)]
+            },
+            "properties": {
+                "accountNumber": faker.random.number(),
+                "orderNumber": faker.random.number(),
+                "createdDate": moment(date).format('M/D/YYYY'),
+                "userFirstName": faker.name.firstName(),
+                "userLastName": faker.name.lastName(),
+                "userEmail": faker.internet.email(),
+                "products": [
+                    {
+                        "productId": faker.random.number(),
+                        "productName": btoa(sample(productCollection)),
+                    }
+                ],
+                "orderTotal": faker.commerce.price(22, 9000, 2)
             }
-          ],
-          "orderTotal": faker.commerce.price(22, 9000 , 2)
         }
-      }
 
 
-  return obj;
+    return obj;
 }
 
 
 const generateOrders = (arr) => {
-  let orders = [];
-  var a = new Date('2018-01-01');
-  var b = new Date('2018-12-31');
-  for (let i = 0; i < 3; i++) {
-    for (var m = moment(a); m.diff(b, 'days') <= 0; m.add(1, 'days')) {
-      orders.push(order(arr[i], m.format('M/DD/YYYY')));
-    }
-  }
+    let orders = [];
+    var a = new Date('2018-01-01');
+    var b = new Date('2018-12-31');
 
-  return orders;
+    arr.forEach((item) => {
+        for (let i = 0; i < item.lp; i++) {
+            //console.log(i)
+            //console.log(item.lp)
+            for (let m = moment(a); m.diff(b, 'days') <= 1; m.add(1, 'days')) {
+                //console.log(moment(m).format('M/DD/YYYY'))
+
+                orders.push(order(item, m));
+            }
+        }
+    });
+
+
+    return orders;
 }
 
 // Run this from the terminal (cd into the generator directory)
 // node index.js | pbcopy
 
 const geoJsonTemplate = () => {
-  return {
-  "type": "FeatureCollection",
-  "features": generateOrders(boundingBoxArray)
-  }
+    return {
+        "type": "FeatureCollection",
+        "features": generateOrders(boundingBoxArray)
+    }
 }
 
+
+//geoJsonTemplate();
 console.log(JSON.stringify(geoJsonTemplate()));
