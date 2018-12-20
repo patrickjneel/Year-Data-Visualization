15a4d6454bf4122389c02dc8b456b6d576cd797a
Pat Neel <neelp@colorado.edu>
2018-12-20 13:41:33 -0700

    add more data points

--- src/db/generator/index.js
+++ src/db/generator/index.js
@@ -11,6 +11,18 @@ const { productCollection } = require('./productsCollection');
 const PRECISION = 0.01;
 
 const boundingBoxArray = [
+  {
+        lat: {
+          min: -23.18,
+          max: -33.54
+        },
+        long: {
+            min: -48.75,
+            max: -71.164
+        },
+      name: 'SA Bottom',
+      lp: 3
+    },
     {
         lat: {
           min: 39.18,
@@ -21,7 +33,7 @@ const boundingBoxArray = [
             max: -93.164
         },
       name: 'US Main',
-      featureCount: 1200
+      lp: 5
     },
     {
         lat: {
@@ -33,7 +45,7 @@ const boundingBoxArray = [
             max: 146.164
         },
       name: 'AUS Main',
-      featureCount: 500
+      lp: 3
     },
     {
         lat: {
@@ -45,19 +57,7 @@ const boundingBoxArray = [
             max: 153.164
         },
       name: 'AUS Bottom',
-      featureCount: 500
-    },
-    {
-        lat: {
-          min: -23.18,
-          max: -33.54
-        },
-        long: {
-            min: -48.75,
-            max: -71.164
-        },
-      name: 'SA Bottom',
-      featureCount: 500
+      lp: 4
     },
 ]
 
@@ -102,13 +102,10 @@ const order = (bbItem, date) => {
 
 const generateOrders = (arr) => {
   let orders = [];
-  var a = moment('2018-01-01');
-  var b = moment('2018-12-31');
-
-  for (let i = 0; i < Math.ceil(arr.featureCount/365); i++) {
-
+  var a = new Date('2018-01-01');
+  var b = new Date('2018-12-31');
+  for (let i = 0; i < 3; i++) {
     for (var m = moment(a); m.diff(b, 'days') <= 0; m.add(1, 'days')) {
-      console.log(m.format('M/DD/YYYY'));
       orders.push(order(arr[i], m.format('M/DD/YYYY')));
     }
   }
@@ -119,10 +116,11 @@ const generateOrders = (arr) => {
 // Run this from the terminal (cd into the generator directory)
 // node index.js | pbcopy
 
-const geoJsonTemplate = {
+const geoJsonTemplate = () => {
+  return {
   "type": "FeatureCollection",
   "features": generateOrders(boundingBoxArray)
+  }
 }
 
-
-console.log(JSON.stringify(geoJsonTemplate));
+console.log(JSON.stringify(geoJsonTemplate()));
